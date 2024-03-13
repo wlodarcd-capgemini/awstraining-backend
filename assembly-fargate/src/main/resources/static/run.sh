7f1e12ef95c3657f82ab4515a1ffb2f2448a3b1b
Damian Cywinski <damian.cywinski@capgemini.com>
2024-03-13 10:48:23 +0100

    Fixed logging.

--- assembly-fargate/src/main/resources/static/run.sh
+++ assembly-fargate/src/main/resources/static/run.sh
@@ -28,5 +28,6 @@ if [[ $HUB != "NONE" ]]; then
     LOG4JFILE="log4j2.xml"
 fi
 
+echo "Running JAVA application..."
 set -x
-sh -c "java -Xms3072m -Xmx3072m -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:ConcGCThreads=8 -XX:ParallelGCThreads=8 -Djavax.net.ssl.trustStore=$HOME/config/$HUB-$STAGE/truststore.jks -Djavax.net.ssl.trustStorePassword=paaspass -Dcom.awstraining.backend.staging.hub=$HUB -Dcom.awstraining.backend.staging.environment=$STAGE -Dspring.profiles.active=$HUB-$STAGE $PARAMSTORE_ARGS -jar app.jar --logging.config=file:./config/$HUB-$STAGE/$LOG4JFILE"
+sh -c "java -Xms512m -Xmx1024m -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:ConcGCThreads=8 -XX:ParallelGCThreads=8 -Djavax.net.ssl.trustStore=$HOME/config/$HUB-$STAGE/truststore.jks -Djavax.net.ssl.trustStorePassword=paaspass -Dcom.awstraining.backend.staging.hub=$HUB -Dcom.awstraining.backend.staging.environment=$STAGE -Dspring.profiles.active=$HUB-$STAGE $PARAMSTORE_ARGS -jar app.jar --logging.config=file:./config/$HUB-$STAGE/$LOG4JFILE"
