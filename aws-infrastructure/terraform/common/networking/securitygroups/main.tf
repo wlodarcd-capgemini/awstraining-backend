9f79baa4036f022b91726f66a5e15e101434f584
Damian Cywinski <damian.cywinski@capgemini.com>
2024-03-13 17:51:44 +0100

    Further implementation.

--- aws-infrastructure/terraform/common/networking/securitygroups/main.tf
+++ aws-infrastructure/terraform/common/networking/securitygroups/main.tf
@@ -52,12 +52,20 @@ resource "aws_security_group" "sg_backend" {
     cidr_blocks = [ data.terraform_remote_state.vpc.outputs.vpc_cidr ]
   }
 
+  ingress {
+    description = "Access to Kibana"
+    from_port = 443
+    protocol = "tcp"
+    to_port = 443
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
   egress {
     from_port = 0
     to_port = 0
     protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    cidr_blocks = [ "0.0.0.0/0" ]
+    ipv6_cidr_blocks = [ "::/0" ]
   }
 
   tags = merge(
