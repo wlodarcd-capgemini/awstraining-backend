cdb115a3525200b66cccfcbe29dea89763e9a86e
Tomasz Wasielewski <tomasz.wasielewski@capgemini.com>
2024-03-19 14:03:35 +0100

    arn secret manager added

--- aws-infrastructure/terraform/modules/sns/main.tf
+++ aws-infrastructure/terraform/modules/sns/main.tf
@@ -14,6 +14,6 @@ resource "aws_kms_key" "aws_backend_sns_key" {
 }
 
 resource "aws_kms_alias" "backend_kms_alias" {
-  name          = "alias/aws-backend-sns-key"
+  name          = "alias/aws-backend-sns-key-2"
   target_key_id = aws_kms_key.aws_backend_sns_key.arn
 }
