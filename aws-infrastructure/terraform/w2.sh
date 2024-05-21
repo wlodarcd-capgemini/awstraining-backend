8509afbe2c1ffa21e2bda53d1e964f4c6a02d46c
Damian Cywinski <damian.cywinski@capgemini.com>
2024-05-21 22:17:14 +0200

    Further fixes to the Terraform

--- aws-infrastructure/terraform/w2.sh
+++ aws-infrastructure/terraform/w2.sh
@@ -161,9 +161,9 @@ tf_file_name="$(basename ${target_path})"
 state_path="${tf_file_name}.tfstate"
 
 terraform get
-terraform init -backend-config "bucket=${TF_VAR_remote_state_bucket}" -backend-config "key=${state_path}" -backend-config "region=${region}" -backend-config "profile=${profile}" -var environment=${environment} -var profile=${profile} -var remote_state_bucket=${TF_VAR_remote_state_bucket} -var region=${region} -var shared_credentials_file="${UNIQUE_BUCKET_STRING}" -lock=true
+terraform init -backend-config "bucket=${TF_VAR_remote_state_bucket}" -backend-config "key=${state_path}" -backend-config "region=${region}" -backend-config "profile=${profile}" -var environment=${environment} -var profile=${profile} -var remote_state_bucket=${TF_VAR_remote_state_bucket} -var region=${region} -var shared_credentials_file="${SHARED_CREDENTIALS_FILE}" -lock=true
 if [ "$command" != "init" ]
 then
   echo "Running terraform command: $command"
-  terraform ${command} -var remote_state_bucket=${TF_VAR_remote_state_bucket} -var region=${region} -var environment=${environment} -var profile=${profile} -var shared_credentials_file=${UNIQUE_BUCKET_STRING} ${options}
+  terraform ${command} -var remote_state_bucket=${TF_VAR_remote_state_bucket} -var region=${region} -var environment=${environment} -var profile=${profile} -var shared_credentials_file=${SHARED_CREDENTIALS_FILE} ${options}
 fi
