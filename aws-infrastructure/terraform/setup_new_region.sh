e999ba6f28a42f98abf5cd5e551b9deb1a0c76a3
Damian Cywinski <damian.cywinski@capgemini.com>
2024-05-21 21:59:53 +0200

    Further fixes to the Terraform

--- aws-infrastructure/terraform/setup_new_region.sh
+++ aws-infrastructure/terraform/setup_new_region.sh
@@ -1,6 +1,56 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 set -e
+
+# Load properties file
+source 'wrapper.properties'
+
+# Declare an associative array
+declare -A REGION_TO_HUB=(
+  ["eu-central-1"]="emea"
+  ["us-east-1"]="us"
+  ["cn-north-1"]="cn"
+)
+
+SCRIPT=$1
+PROFILE=$2
+REGION=$3
+# Get the corresponding hub from the associative array
+HUB="${REGION_TO_HUB[$REGION]}"
+ACTION=${@:4}
+
+TF_STATE_BUCKET="tf-state-${PROFILE}-${REGION}-${UNIQUE_BUCKET_STRING}"
+
+empty_tfstate_bucket() {
+  aws s3api delete-objects \
+      --bucket $TF_STATE_BUCKET \
+      --delete "$(aws s3api list-object-versions --bucket ${TF_STATE_BUCKET} --output=json --query='{Objects: Versions[].{Key:Key,VersionId:VersionId}}')" \
+      --profile $PROFILE \
+      --region $REGION || true
+}
+
+delete_secrets_manager() {
+  aws secretsmanager delete-secret \
+      --secret-id backend-secretsmanager-test-eu-central-1 \
+      --force-delete-without-recovery \
+    	--profile $PROFILE \
+    	--region $REGION || true
+}
+
+empty_ecr() {
+  ECR_REPOSITORY="backend"
+  aws ecr batch-delete-image \
+      --repository-name $ECR_REPOSITORY \
+      --profile $PROFILE \
+      --region $REGION \
+      --image-ids "$(aws ecr list-images --region $REGION --profile $PROFILE --repository-name $ECR_REPOSITORY --query 'imageIds[*]' --output json)" || true
+}
+
+delete_log_groups() {
+  aws logs describe-log-groups --query 'logGroups[*].logGroupName' --output table --region $REGION --profile $PROFILE | \
+  awk '{print $2}' | grep -v ^$ | while read x; do  echo "deleting $x" ; aws logs delete-log-group --log-group-name $x --region $REGION --profile $PROFILE; done || true
+}
+
 if [ "$#" -lt 4 ]; then
   echo "Not enough arguments provided."
   echo
@@ -24,33 +74,26 @@ fi
 # remove any state from previous runs (possibly on different environments)
 rm common/*/*/.terraform/terraform.tfstate || true
 
-# Declare an associative array
-declare -A REGION_TO_HUB=(
-  ["eu-central-1"]="emea"
-  ["us-east-1"]="us"
-  ["cn-north-1"]="cn"
-)
-
-SCRIPT=$1
-PROFILE=$2
-REGION=$3
-# Get the corresponding hub from the associative array
-HUB="${REGION_TO_HUB[$REGION]}"
-ACTION=${@:4},
-
 if [ "$ACTION" = "destroy -auto-approve" ]; then
-  ./$SCRIPT $PROFILE $REGION common/monitoring/ecs-monitoring-service $ACTION
-  ./$SCRIPT $PROFILE $REGION common/monitoring/ecs-monitoring-cluster $ACTION
-  ./$SCRIPT $PROFILE $REGION common/services/measurements-dynamodb $ACTION
-  ./$SCRIPT $PROFILE $REGION common/services/ecs-backend-service $ACTION
-  ./$SCRIPT $PROFILE $REGION common/services/ecs-backend-cluster $ACTION
-  ./$SCRIPT $PROFILE $REGION common/services/ecr $ACTION
-  ./$SCRIPT $PROFILE $REGION common/monitoring/sns $ACTION
-  ./$SCRIPT $PROFILE $REGION common/networking/securitygroups $ACTION
-  ./$SCRIPT $PROFILE $REGION common/networking/vpc $ACTION
-  ./$SCRIPT $PROFILE $REGION environments/$PROFILE/$HUB/$REGION/globals $ACTION
-  ./$SCRIPT $PROFILE $REGION common/general/dynamo-lock $ACTION
-  ./$SCRIPT $PROFILE $REGION common/general/create-remote-state-bucket $ACTION
+  echo "Checking if $TF_STATE_BUCKET exists..."
+  if aws s3api head-bucket --bucket $TF_STATE_BUCKET --profile $PROFILE --region $REGION 2>/dev/null; then
+    ./$SCRIPT $PROFILE $REGION common/services/measurements-dynamodb $ACTION
+    delete_secrets_manager
+    empty_ecr
+    ./$SCRIPT $PROFILE $REGION common/services/ecs-backend-service $ACTION
+    ./$SCRIPT $PROFILE $REGION common/services/ecs-backend-cluster $ACTION
+    ./$SCRIPT $PROFILE $REGION common/services/ecr $ACTION
+    ./$SCRIPT $PROFILE $REGION common/monitoring/sns $ACTION
+    ./$SCRIPT $PROFILE $REGION common/networking/securitygroups $ACTION
+    ./$SCRIPT $PROFILE $REGION common/networking/vpc $ACTION
+    ./$SCRIPT $PROFILE $REGION environments/$PROFILE/$HUB/$REGION/globals $ACTION
+    ./$SCRIPT $PROFILE $REGION common/general/dynamo-lock $ACTION
+    empty_tfstate_bucket
+    ./$SCRIPT $PROFILE $REGION common/general/create-remote-state-bucket $ACTION
+    delete_log_groups
+  else
+    echo "Skipping destroy - everything was already destroyed!"
+  fi
 else
   ./$SCRIPT $PROFILE $REGION common/general/create-remote-state-bucket $ACTION
   ./$SCRIPT $PROFILE $REGION common/general/dynamo-lock $ACTION
@@ -62,6 +105,4 @@ else
   ./$SCRIPT $PROFILE $REGION common/services/ecs-backend-cluster $ACTION
   ./$SCRIPT $PROFILE $REGION common/services/ecs-backend-service $ACTION
   ./$SCRIPT $PROFILE $REGION common/services/measurements-dynamodb $ACTION
-  ./$SCRIPT $PROFILE $REGION common/monitoring/ecs-monitoring-cluster $ACTION
-  ./$SCRIPT $PROFILE $REGION common/monitoring/ecs-monitoring-service $ACTION
 fi
