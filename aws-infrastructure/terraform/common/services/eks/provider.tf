3c9cbfff104a7092cd52ad243be40f23b810faa9
Damian Cywinski <damian.cywinski@capgemini.com>
2024-05-22 16:14:12 +0200

    Fixed script

--- aws-infrastructure/terraform/common/services/eks/provider.tf
+++ aws-infrastructure/terraform/common/services/eks/provider.tf
@@ -3,25 +3,11 @@ provider "aws" {
   profile = var.aws_profile_name
 }
 
-provider "kubernetes" {
-  config_path = "~/.kube/config"
-}
-
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
-}
-
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "5.40.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "2.27.0"
     }
     helm = {
       source  = "hashicorp/helm"
