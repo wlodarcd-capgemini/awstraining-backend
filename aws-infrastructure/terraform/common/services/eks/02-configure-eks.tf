b47c26bd2eb75c9c82f431db5b79096cc3e0c2ec
Damian Cywinski <damian.cywinski@capgemini.com>
2024-05-22 12:18:55 +0200

    Fixed run local-exec command

--- aws-infrastructure/terraform/common/services/eks/02-configure-eks.tf
+++ aws-infrastructure/terraform/common/services/eks/02-configure-eks.tf
@@ -2,6 +2,6 @@ resource "null_resource" "next" {
   depends_on = [module.eks]
   provisioner "local-exec" {
     command     = "aws eks update-kubeconfig --name ${module.eks.cluster_name} --profile ${var.aws_profile_name} --region ${var.region}"
-    interpreter = ["PowerShell", "-Command"]
+    interpreter = ["/bin/bash", "-c"]
   }
 }
