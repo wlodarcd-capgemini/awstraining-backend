63de3a7aed7b5ede2e3028001928ece626eef3ee
Damian Cywinski <damian.cywinski@capgemini.com>
2024-05-23 10:24:24 +0200

    Fixes to Terraform code

--- aws-infrastructure/terraform/common/services/eks/modules/aws-alb-controller/main.tf
+++ aws-infrastructure/terraform/common/services/eks/modules/aws-alb-controller/main.tf
@@ -39,43 +39,43 @@ resource "kubernetes_service_account" "service-account" {
 ## Install Load Balancer Controler With Helm
 #################################################################################
 #
-resource "helm_release" "lb" {
-  name       = "aws-load-balancer-controller"
-  repository = "https://aws.github.io/eks-charts"
-  chart      = "aws-load-balancer-controller"
-  namespace  = "kube-system"
-  depends_on = [
-    kubernetes_service_account.service-account
-  ]
-
-  set {
-    name  = "region"
-    value = var.region
-  }
-
-  set {
-    name  = "vpcId"
-    value = var.vpc_id
-  }
-
-  set {
-    name  = "image.repository"
-    value = "602401143452.dkr.ecr.${var.region}.amazonaws.com/amazon/aws-load-balancer-controller"
-  }
-
-  set {
-    name  = "serviceAccount.create"
-    value = "false"
-  }
-
-  set {
-    name  = "serviceAccount.name"
-    value = "aws-load-balancer-controller"
-  }
-
-  set {
-    name  = "clusterName"
-    value = var.cluster_name
-  }
-}
-
+#resource "helm_release" "lb" {
+#  name       = "aws-load-balancer-controller"
+#  repository = "https://aws.github.io/eks-charts"
+#  chart      = "aws-load-balancer-controller"
+#  namespace  = "kube-system"
+#  depends_on = [
+#    kubernetes_service_account.service-account
+#  ]
+#
+#  set {
+#    name  = "region"
+#    value = var.region
+#  }
+#
+#  set {
+#    name  = "vpcId"
+#    value = var.vpc_id
+#  }
+#
+#  set {
+#    name  = "image.repository"
+#    value = "602401143452.dkr.ecr.${var.region}.amazonaws.com/amazon/aws-load-balancer-controller"
+#  }
+#
+#  set {
+#    name  = "serviceAccount.create"
+#    value = "false"
+#  }
+#
+#  set {
+#    name  = "serviceAccount.name"
+#    value = "aws-load-balancer-controller"
+#  }
+#
+#  set {
+#    name  = "clusterName"
+#    value = var.cluster_name
+#  }
+#}
+#
